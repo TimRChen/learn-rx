f9021bef2521e887a0d4ea891f1b35cee4123208
TimRChen <timrchen@foxmail.com>
2020-01-15 15:31:02 +0800

    删除无用设计

--- src/dragShadow.js
+++ src/dragShadow.js
@@ -1,6 +1,29 @@
 import { fromEvent, from, interval, zip } from "rxjs";
 import { map, switchMap, takeUntil, mergeMap, tap } from "rxjs/operators";
-import { setTranslate, getTranslate } from "./utils";
+
+function getTranslate(element) {
+  const style = getComputedStyle(element);
+  const regExp = /matrix\((\d+,\s){4}(\d+),\s(\d+)/i;
+  const result = style.transform.match(regExp)
+  if (result) {
+    const [,,x,y] = result
+    return {
+      x: parseInt(x, 10),
+      y: parseInt(y, 10)
+    };
+  } else {
+    return {
+      x: 0,
+      y: 0
+    };
+  }
+}
+
+function setTranslate(element, pos) {
+  const { x, y } = pos;
+  element.style.transform = `translate(${x}px, ${y}px)`;
+}
+
 
 const headBox = document.querySelector(".head-box");
 const boxes = document.querySelectorAll(".box");
